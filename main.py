34ad234d570058cc116eb2e8ecc042d7e65c9210
Mohammad Shirmohammadi <141566729+mohammadshirmohammadi589@users.noreply.github.com>
2025-10-19 16:04:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ warnings.filterwarnings('ignore')  # برای جلوگیری از warningهای 
 
 # 🔐 Credentials
 CREDENTIALS = {
-    'DataVisionary': 'Data$code2024!'
+    'Datavisionary': 'Datacode2024'
 }
 USERS_FILE = 'users_usage.json'
 
@@ -277,4 +277,5 @@ elif menu == "آپلود":
         st.write("داده‌ها:", df_up.head())
 
         # predict batch اگر خواستی اضافه کن
+
 
